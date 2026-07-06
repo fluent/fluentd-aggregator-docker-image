7ff0ee1ef3943f3acfa3662e6b71fe280d1580e8
Steve Hipwell <steve.hipwell@gmail.com>
2026-07-06 13:32:07 +0100

    feat: Refactor image to v2
    Signed-off-by: Steve Hipwell <steve.hipwell@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,11 @@
 #source vars if file exists
 DEFAULT=/etc/default/fluentd
 
-if [ -r $DEFAULT ]; then
-    set -o allexport
-    . $DEFAULT
-    set +o allexport
+if [ -r "${DEFAULT}" ]; then
+  set -o allexport
+  # shellcheck disable=SC1090
+  . "${DEFAULT}"
+  set +o allexport
 fi
 
 # If the user has supplied only arguments append them to `fluentd` command
@@ -16,13 +17,13 @@ fi
 
 # If user does not supply config file or plugins, use the default
 if [ "$1" = "fluentd" ]; then
-    if ! echo $@ | grep -e ' \-c' -e ' \-\-config' ; then
-       set -- "$@" --config /fluentd/etc/${FLUENTD_CONF}
-    fi
+  if ! echo "$@" | grep -e ' \-c' -e ' \-\-config' ; then
+    set -- "$@" --config "/fluentd/etc/${FLUENTD_CONF:-}"
+  fi
 
-    if ! echo $@ | grep -e ' \-p' -e ' \-\-plugin' ; then
-       set -- "$@" --plugin /fluentd/plugins
-    fi
+  if ! echo "$@" | grep -e ' \-p' -e ' \-\-plugin' ; then
+    set -- "$@" --plugin /fluentd/plugins
+  fi
 fi
 
 exec "$@"
